e2f92c07021406f78edd29d7318b7539ed29b2ad
Facundo Cingolani <mcingolani28@gmail.com>
2025-07-09 16:48:40 -0300

    Fix mensaje error propiedad no disponible

--- src/web/controllers/reservas.py
+++ src/web/controllers/reservas.py
@@ -78,7 +78,10 @@ def create_reserva():
         if reservas.hay_reservas_solapadas(
                 data_reserva['id_propiedad'], data_reserva['fecha_inicio'], data_reserva['fecha_fin']
                 ):
-            return {'error': '¡Casi! Esta propiedad acaba de ser ocupada.'}, 400
+            return {
+                    'title': '¡Casi!',
+                    'error': 'Esta propiedad acaba de ser ocupada.'
+                    }, 400
         reserva = reservas.create_reserva(data_reserva)
         # Generar datos necesarios para el email
         data_email = reservas.get_schema_email_reserva().dump(reserva)
